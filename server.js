a5de612ca46fff72815c5d463265c4f37bc34c28
Ben Manning <bmanning@me.com>
2016-07-29 16:56:27 -0700

    add seed data for route testing

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ var app = express();
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
+// seed posts
+var posts = [
+  {
+    title: "This is the first title",
+    body:  "This is the first post that this website is dealing with.",
+    createdAt: "This is the Date of this post."
+  },
+  {
+    title: "This is the second title for things.",
+    body: "This is the second post that is being created.  Woo!!!!",
+    createdAt: "This is the Date of this post."
+  },
+  {
+    title: "This is the third title",
+    body: "This is another post for the band.",
+    createdAt: "This is the Date of this post"
+  }
+];
 
 // set the port to listen for the app
 app.listen(process.env.PORT || 3000, function(){
