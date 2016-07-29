6be764a781487c12faee896bec56e16f36f94031
Ben Manning <bmanning@me.com>
2016-07-29 16:37:25 -0700

    last commit before going home

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
 // set the port to listen for the app
 app.listen(process.env.PORT || 3000, function(){
   console.log("I am listening");
